a64c081b97de10c3ed935471ff9306e7be594169
5000vw <isaackiplangat1999@gmail.com>
2023-11-16 16:30:00 +0300

    implement responsiveness of footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,52 +1,53 @@
-import React from 'react'
-import Logo from '../../assets/logo.jpg'
+import React from 'react';
+import Logo from '../../assets/logo.jpg';
 
 const Footer = () => {
   return (
-    <div className='w-4/5 mx-auto grid grid-cols-5 gap-5 mb-5'>
+    <div className='w-4/5 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mb-5'>
+      {/* Logo and Info */}
+      <div>
+        <img src={Logo} alt="logo" width={50} height={50} className='ml-3 py-2' />
+        <p className='text-sm py-1'>@2023 Nobles Church</p>
+        <p className='text-sm py-1'>info@nobleschurch.com</p>
+        <p className='text-sm'>Website Created <br /> by <span className='font-bold'>VerseWrld</span></p>
+      </div>
 
-    <div>
-    <img src={Logo} alt="logo"  width={50} height={50} className='ml-3 py-2' />
-    <p className='text-sm py-1'>@2023 Nobles Church</p>
-    <p className='text-sm py-1'>info@nobleschurch.com</p>
-    <p className='text-sm'>Website Created <br/> by <span className='font-bold'>VerseWrld</span></p>
+      {/* Get Involved */}
+      <div className='capitalize'>
+        <h2 className='text-md font-semibold py-2'>Get Involved</h2>
+        <p className='text-sm py-1'>Nobles Serve Team</p>
+        <p className='text-sm py-1'>Connect Groups</p>
+        <p className='text-sm py-1 '>Culture Class</p>
+      </div>
+
+      {/* Ministries */}
+      <div className='capitalize'>
+        <h2 className='text-md font-semibold py-2'>Ministries</h2>
+        <p className='text-sm py-1'>Noble Kids</p>
+        <p className='text-sm py-1'>Noble Leadership</p>
+        <p className='text-sm py-1 '>Nobles Mums</p>
+        <p className='text-sm py-1 '>Events</p>
+        <p className='text-sm py-1 '>Noble Projects</p>
+      </div>
+
+      {/* Resources */}
+      <div className='capitalize'>
+        <h2 className='text-md font-semibold py-2'>Resources</h2>
+        <p className='text-sm py-1'>Give</p>
+        <p className='text-sm py-1'>Noble Shop</p>
+      </div>
+
+      {/* Media */}
+      <div className='capitalize'>
+        <h2 className='text-md font-semibold py-2 '>Media</h2>
+        <p className='text-sm py-1'>Sunday Services</p>
+        <p className='text-sm py-1'>Nobles Youtube</p>
+        <p className='text-sm py-1 '>Nobles Music</p>
+        <p className='text-sm py-1 '>Nobles Podcast</p>
+        <p className='text-sm py-1 '>Leadership Academy</p>
+      </div>
     </div>
+  );
+};
 
-    <div className='capitalize'>
-    <h2 className='text-md font-semibold py-2'>Get Involved</h2>
-    <p className='text-sm py-1'>Nobles Serve Team</p>
-    <p className='text-sm py-1'>Connect Groups</p>
-    <p className='text-sm py-1 '>Culture Class</p>
-    </div>
-
-    <div className='capitalize'>
-    <h2 className='text-md font-semibold py-2'>ministries</h2>
-    <p className='text-sm py-1'>Noble Kids</p>
-    <p className='text-sm py-1'>noble leadership</p>
-    <p className='text-sm py-1 '>nobles mums</p>
-    <p className='text-sm py-1 '>events</p>
-    <p className='text-sm py-1 '>noble projects</p>
-
-    </div>
-
-    <div className='capitalize'>
-    <h2 className='text-md font-semibold py-2'>resources</h2>
-    <p className='text-sm py-1'>give</p>
-    <p className='text-sm py-1'>noble shop</p>
-    </div>
-
-    <div className='capitalize'>
-    <h2 className='text-md font-semibold py-2 '>Media</h2>
-    <p className='text-sm py-1'>Sunday services</p>
-    <p className='text-sm py-1'>Nobles Youtube</p>
-    <p className='text-sm py-1 '>Nobles music</p>
-    <p className='text-sm py-1 '>Nobles Podcast</p>
-    <p className='text-sm py-1 '>Leadership academy</p>
-
-    </div>
-
-    </div>
-  )
-}
-
-export default Footer
+export default Footer;
--- src/Components/Services/Giving.jsx
+++ src/Components/Services/Giving.jsx
@@ -1,48 +1,52 @@
-import React from 'react'
-import { FaChevronRight, FaWhatsapp } from 'react-icons/fa'
-import {MdMonitor} from 'react-icons/md'
-import {IoIosPhonePortrait} from 'react-icons/io'
+import React from 'react';
+import { FaChevronRight, FaWhatsapp } from 'react-icons/fa';
+import { MdMonitor } from 'react-icons/md';
+import { IoIosPhonePortrait } from 'react-icons/io';
 
 const Giving = () => {
   return (
     <div className='w-4/5 py-10 mx-auto'>
-    <div className='py-10 bg-gradient-to-b  from-green-500 via-violet-400 to-purple-600 mx-auto'>
-    <h2 className='text-center text-white font-semibold text-5xl capitalize'> ways we  give</h2>
-    <p className='text-sm capitalize text-white text-center py-4'>patner with Fountain Family</p>
-    <div className='w-2/3 grid grid-cols-2 mx-auto gap-5'>
-    
-    <div className='border-2 border-white text-white bg-transparent flex justify-between items-center px-4 py-2 rounded-lg'>
-    <h2 className='font-medium text-lg w-2/5 '>Your Generation at work</h2>
-    <FaChevronRight/>
-    </div>
+      <div className='py-10 bg-gradient-to-b  from-green-500 via-violet-400 to-purple-600 mx-auto'>
+        <h2 className='text-center text-white font-semibold text-3xl md:text-5xl capitalize'>Ways We Give</h2>
+        <p className='text-sm capitalize text-white text-center py-4'>Partner with Fountain Family</p>
+        <div className='w-2/3 grid grid-cols-1 md:grid-cols-2 mx-auto gap-5'>
 
-    <div className='border-2 border-white text-white bg-transparent flex justify-between items-center px-4 py-2 rounded-lg'>
-    <h2 className='font-medium text-lg w-1/2  p-2 '>Become a Kingdom Builder</h2>
-    <FaChevronRight/>
-    </div>
-    </div>
+          {/* Option 1 */}
+          <div className='border-2 border-white text-white bg-transparent flex justify-between items-center px-4 py-2 rounded-lg mb-4'>
+            <h2 className='font-medium text-lg w-1/2 md:w-full p-2'>Your Generation at Work</h2>
+            <FaChevronRight />
+          </div>
 
-    <div className='w-1/3 mx-auto flex justify-between py-10 text-white text-sm'>
-    
-    <div className=''>
-    <MdMonitor size={40} className='text-white'/>
-    <p>Online</p>
-    </div>
-    <div className='text-center'>
-    <IoIosPhonePortrait size={40} className='text-white'/>
-    <p>App</p>
-    </div>
-    <div className='text-center'>
-    <FaWhatsapp size={40} className='text-white mx-2'/>
-    <p className='text-center'>Channel</p>
-    </div>
+          {/* Option 2 */}
+          <div className='border-2 border-white text-white bg-transparent flex justify-between items-center px-4 py-2 rounded-lg mb-4'>
+            <h2 className='font-medium text-lg w-1/2 md:w-full p-2'>Become a Kingdom Builder</h2>
+            <FaChevronRight />
+          </div>
+        </div>
 
-    
+        <div className='w-full md:w-1/2 mx-auto sm:w-full sm:mx-auto flex  items-center justify-around py-10 text-white text-sm'>
+        {/* Online */}
+        <div className='text-center mb-4'>
+          <MdMonitor size={40} className='text-white' />
+          <p>Online</p>
+        </div>
+      
+        {/* App */}
+        <div className='text-center mb-4'>
+          <IoIosPhonePortrait size={40} className='text-white' />
+          <p>App</p>
+        </div>
+      
+        {/* WhatsApp Channel */}
+        <div className='text-center mb-4'>
+          <FaWhatsapp size={40} className='text-white ' />
+          <p className='text-center'>Channel</p>
+        </div>
+      </div>
+      
+      </div>
     </div>
-    </div>
-
-    </div>
-  )
+  );
 }
 
-export default Giving
+export default Giving;
