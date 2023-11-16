3c2c504f5b53c46596056dc521fec93cd9023360
5000vw <isaackiplangat1999@gmail.com>
2023-11-16 18:06:49 +0300

    correct styling errors

--- src/Components/Services/GetInvolved.jsx
+++ src/Components/Services/GetInvolved.jsx
@@ -2,38 +2,38 @@ import React from 'react';
 
 const GetInvolved = () => {
   return (
-    <div  classNameName='w-4/5 mx-auto py-10'>
-      <div  classNameName='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5'>
-        <h2  classNameName='text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-semibold text-center sm:text-left mb-4'>
+    <div  className='w-4/5 mx-auto py-10'>
+      <div  className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+        <h2  className='text-3xl sm:text-4xl md:text-4xl lg:text-5xl font-semibold text-center sm:text-left mb-4'>
           Get Involved
         </h2>
-        <p  classNameName='w-full sm:w-2/3 mx-auto text-center sm:text-left'>
+        <p  className='w-full sm:w-2/3 mx-auto text-center sm:text-left'>
           This is how we do community. Wherever you are on your faith journey, there is a place for you to get involved.
         </p>
       </div>
 
       {/* Event cards */}
-      <div  classNameName='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 pt-10'>
-        <div  classNameName='bg-gradient-to-br from-pink-400 to bg-amber-300 px-6 sm:px-10 py-12'>
-          <h2  classNameName='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
+      <div  className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 pt-10'>
+        <div  className='bg-gradient-to-br from-pink-400 to bg-amber-300 px-6 sm:px-10 py-12'>
+          <h2  className='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
             Sunday Services
           </h2>
         </div>
 
-        <div  classNameName='bg-black px-6 sm:px-10 py-12'>
-          <h2  classNameName='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
+        <div  className='bg-black px-6 sm:px-10 py-12'>
+          <h2  className='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
             Sunday Services
           </h2>
         </div>
 
-        <div  classNameName='bg-gradient-to-b from-purple-600 to bg-pink-400 px-6 sm:px-10 py-12'>
-          <h2  classNameName='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
+        <div  className='bg-gradient-to-b from-purple-600 to bg-pink-400 px-6 sm:px-10 py-12'>
+          <h2  className='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
             Connect Groups
           </h2>
         </div>
 
-        <div  classNameName='bg-gradient-to-br from-green-400 to bg-blue-400 px-6 sm:px-10 py-12'>
-          <h2  classNameName='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
+        <div  className='bg-gradient-to-br from-green-400 to bg-blue-400 px-6 sm:px-10 py-12'>
+          <h2  className='text-center text-white font-semibold text-3xl sm:text-4xl uppercase'>
             Culture  className
           </h2>
         </div>
--- src/Components/Services/Groups.jsx
+++ src/Components/Services/Groups.jsx
@@ -3,34 +3,34 @@ import NobleKids from '../../assets/image1.jpg';
 
 const Groups = () => {
   return (
-    <div  classNameName='w-4/5 py-5 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-5'>
+    <div  className='w-4/5 py-5 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-5'>
 
       {/* Group 1 */}
-      <div style={{ backgroundImage: `url(${NobleKids})` }}  classNameName='text-white'>
-        <div  classNameName='bg-black opacity-60 grid px-6 sm:px-10 py-16'>
-          <h2  classNameName='text-center sm:text-left text-3xl sm:text-5xl font-semibold py-2 sm:py-5'>
+      <div style={{ backgroundImage: `url(${NobleKids})` }}  className='text-white'>
+        <div  className='bg-black opacity-60 grid px-6 sm:px-10 py-16'>
+          <h2  className='text-center sm:text-left text-3xl sm:text-5xl font-semibold py-2 sm:py-5'>
             Noble Kids
           </h2>
-          <p  classNameName='w-full mx-auto p-2 text-center sm:text-left text-sm sm:text-md'>
+          <p  className='w-full mx-auto p-2 text-center sm:text-left text-sm sm:text-md'>
             At Noble Kids, we offer a safe, fun, and age-appropriate environment for kids to learn about God both online and in person. Our mission every Sunday is to create an atmosphere where your kids can grow more in their relationship with Jesus.
           </p>
-          <div  classNameName='mx-auto py-4'>
-            <button  classNameName='border border-white rounded-lg px-4 py-2'>Learn more</button>
+          <div  className='mx-auto py-4'>
+            <button  className='border border-white rounded-lg px-4 py-2'>Learn more</button>
           </div>
         </div>
       </div>
 
       {/* Group 2 */}
-      <div style={{ backgroundImage: `url(${NobleKids})` }}  classNameName='text-white'>
-        <div  classNameName='bg-black opacity-60 grid px-6 sm:px-10 py-16'>
-          <h2  classNameName='text-center sm:text-left text-3xl sm:text-5xl font-semibold py-2 sm:py-5'>
+      <div style={{ backgroundImage: `url(${NobleKids})` }}  className='text-white'>
+        <div  className='bg-black opacity-60 grid px-6 sm:px-10 py-16'>
+          <h2  className='text-center sm:text-left text-3xl sm:text-5xl font-semibold py-2 sm:py-5'>
             Noble Kids
           </h2>
-          <p  classNameName='w-full mx-auto p-2 text-center sm:text-left text-sm sm:text-md'>
+          <p  className='w-full mx-auto p-2 text-center sm:text-left text-sm sm:text-md'>
             At Noble Kids, we offer a safe, fun, and age-appropriate environment for kids to learn about God both online and in person. Our mission every Sunday is to create an atmosphere where your kids can grow more in their relationship with Jesus.
           </p>
-          <div  classNameName='mx-auto py-4'>
-            <button  classNameName='border border-white rounded-lg px-4 py-2'>Learn more</button>
+          <div  className='mx-auto py-4'>
+            <button  className='border border-white rounded-lg px-4 py-2'>Learn more</button>
           </div>
         </div>
       </div>
